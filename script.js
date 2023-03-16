588f19bf5909802736b849f9c945370c2d311ef8
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-03-16 22:49:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function calculateMinCost() {
-  //your code here
-  
+  var inputData = document.querySelector("input").value;
+	var inputArr = inputData.split(",");
+	for(var i = 0; i < inputArr.length; i++){
+		inputArr[i] = Number(inputArr[i]);
+	}
+	var cost = 0;
+	inputArr.sort(function(a,b) {return a-b});
+	while(inputArr.length > 1){
+		var nr = inputArr[0] + inputArr[1];
+		cost += nr;
+		inputArr.splice(0,2);
+		inputArr.push(nr);
+		inputArr.sort(function(a,b) {return a-b});
+	}
+  document.querySelector("#result").textContent = cost;
   
   
 }  
