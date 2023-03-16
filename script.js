94a6c4a91a204480daf2823a0ba46ec463c9086b
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-03-16 23:10:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function calculateMinCost() {
 		inputArr[i] = Number(inputArr[i]);
 	}
 	var cost = 0;
-	inputArr.sort(function(a,b) {return a-b});
+	//inputArr.sort(function(a,b) {return a-b});
 	while(inputArr.length > 1){
 		var nr = inputArr[0] + inputArr[1];
 		cost += nr;
 		inputArr.splice(0,2);
 		inputArr.push(nr);
-		inputArr.sort(function(a,b) {return a-b});
+		inputArr.sort(function(a,b) {return a-b; });
 	}
-    document.querySelector("result").textContent = cost;
+    document.querySelector("result").textcontent = cost;
 }
